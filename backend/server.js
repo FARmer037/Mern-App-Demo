3916fd01a8edf3886fe120f015aef5d4fdbb6b37
Mustofa <mustofa_sachi@hotmail.com>
2022-03-31 14:51:50 +0700

    REST API for goal

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const colors = require('colors')
 const { errorHandler } = require('./middleware/errorMiddleware')
+const goalRoutes = require('./routers/goalRoutes')
+const connectDB = require('./config/db')
+
 const port = process.env.PORT || 5000
 
-const goalRoutes = require('./routers/goalRoutes')
+connectDB()
 
 const app = express()
 
