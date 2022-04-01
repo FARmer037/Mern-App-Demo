0a6c038c0fb7c9f6e8b575d312c4ad9e0750aaba
Mustofa <mustofa_sachi@hotmail.com>
2022-04-01 19:51:21 +0700

    JWT Authentication

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,11 +1,13 @@
+const { ObjectId } = require('bson')
 const asyncHandler = require('express-async-handler')
 const Goal = require('../models/goalModel')
+const User = require('../models/userModel')
 
 //  @desc   Get goals
 //  @route  GET /api/goals
 //  @access Private
 const getGoals = asyncHandler(async (req, res) => {
-    const goals = await Goal.find()
+    const goals = await Goal.find({ user: req.user.id })
 
     res.json(goals)
 })
@@ -21,7 +23,8 @@ const setGoal = asyncHandler(async (req, res) => {
 
     const goal = await Goal.create(
         {
-            text: req.body.text
+            text: req.body.text,
+            user: req.user.id
         }
     )
 
@@ -37,6 +40,20 @@ const updateGoal = asyncHandler(async (req, res) => {
     if (!goal) {
         res.status(400)
         throw new Error('Goal not found')
+    }
+
+    const user = await User.findById(req.user.id)
+
+    //  Check for user
+    if (!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    //  Make sure the logged in user matches the goal user
+    if (goal.user.toString() !== user.id) {
+        res.status(401)
+        throw new Error('User not authorize')
     }
 
     const updateGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, { new: true })
@@ -55,7 +72,21 @@ const deleteGoal = asyncHandler(async (req, res) => {
         throw new Error('Goal not found')
     }
 
-    await Goal.deleteOne()
+    const user = await User.findById(req.user.id)
+
+    //  Check for user
+    if (!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    //  Make sure the logged in user matches the goal user
+    if (goal.user.toString() !== user.id) {
+        res.status(401)
+        throw new Error('User not authorize')
+    }
+
+    await Goal.deleteOne({ _id: ObjectId(req.params.id) })
 
     res.json({ id: req.params.id })
 })
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv').config()
 const colors = require('colors')
 const { errorHandler } = require('./middleware/errorMiddleware')
 const goalRoutes = require('./routers/goalRoutes')
+const userRoutes = require('./routers/userRoutes')
 const connectDB = require('./config/db')
 
 const port = process.env.PORT || 5000
@@ -15,6 +16,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/goals', goalRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(errorHandler)
 
