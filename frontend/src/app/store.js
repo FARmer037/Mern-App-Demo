3e13dbb76882aedbff149b21143e34953b345476
Mustofa <mustofa_sachi@hotmail.com>
2022-04-04 16:05:16 +0700

    Redux goals

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
-import authReduce from '../features/auth/authSlice';
+import authReduce from '../features/auth/authSlice'
+import goalReduce from '../features/goals/goalSlice'
 
 export const store = configureStore({
   reducer: {
-    auth: authReduce
+    auth: authReduce,
+    goals: goalReduce
   },
 });
