ba3cb65cb5c79e0ff247211f24b6f272c66e9268
Mustofa <mustofa_sachi@hotmail.com>
2022-04-04 11:00:11 +0700

    Authentication with redux toolkit

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import authReduce from '../features/auth/authSlice';
 
 export const store = configureStore({
   reducer: {
-
+    auth: authReduce
   },
 });
